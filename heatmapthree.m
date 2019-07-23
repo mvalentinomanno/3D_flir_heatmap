09f1714656433beda945e33d2de7714d39e291f1
mvalentinomanno <36999369+mvalentinomanno@users.noreply.github.com>
2019-07-23 09:54:42 -0600

    Multiple plots & user input
    I updated the code so that the user enters the .csv file name they want to visualize, and the subsequent number of files to visualize.

--- heatmapthree.m
+++ heatmapthree.m
@@ -1,10 +1,23 @@
 %Michael Valentino-Manno
-%7/22/19
+%7/23/19
 %3D heatmap fun stuff
+%this code is setup for csv files named radiometry#.csv
 %to view data at a certain point, click the 'data cursor' in the plot
 %window, then click on the plot
+cranberry = '\nEnter the name of the first csv file you want to visualize \n(Note: the file name must follow this naming convention: radiometry#.csv | e.g. radiometry45.csv) : ';
+huckleberry = input(cranberry,'s');
 
-raspberry = csvread('radiometry45.csv'); %substitute the name of the csv file you want to plot
+marionberry = char(extractBetween(huckleberry,"radiometry",".csv",'Boundaries','exclusive'));
+
+cranberry = 'How many subsequent csv files would you like to visualize? (enter a number): ';
+huckleberry = input(cranberry) + 1;
+
+for splash = 1:huckleberry
+    
+cherry = strcat('radiometry',marionberry);
+cherry = strcat(cherry,'.csv');
+
+raspberry = csvread(cherry); 
 
 strawberry = size(raspberry,1);
 blueberry = size(raspberry,2);
@@ -16,11 +29,17 @@ for row = 1:strawberry %nested for loop goes through each value
         raspberry(row,col) = grape;
     end
 end
-
+figure
 blackberry = surf(raspberry);
 zlabel('Temperature (C)');
 xlabel('Width Pixel');
 ylabel('Height Pixel');
+title(cherry);
 maple = colorbar;      
 ylabel(maple, 'Temperature (C)')
 colormap('hot'); %'cus you're hot and you're cold
+
+drip = str2num(marionberry);
+drip = drip + 1;
+marionberry = num2str(drip);
+end
